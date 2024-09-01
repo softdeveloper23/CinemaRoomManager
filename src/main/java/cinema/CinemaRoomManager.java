c03001dcee045c784b9e63124638aaf680f132bd
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-01 11:52:24 +0200

    Added seating arrangement

--- src/main/java/cinema/CinemaRoomManager.java
+++ src/main/java/cinema/CinemaRoomManager.java
@@ -2,6 +2,15 @@ package cinema;
 
 public class CinemaRoomManager {
     public static void main(String[] args) {
-        // Write code here
+        // Cinema seating arrangement
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        System.out.println("1 S S S S S S S S");
+        System.out.println("2 S S S S S S S S");
+        System.out.println("3 S S S S S S S S");
+        System.out.println("4 S S S S S S S S");
+        System.out.println("5 S S S S S S S S");
+        System.out.println("6 S S S S S S S S");
+        System.out.println("7 S S S S S S S S");
     }
 }
