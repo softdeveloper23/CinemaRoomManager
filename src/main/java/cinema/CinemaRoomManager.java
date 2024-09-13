d70315345cd64aaf4fbb116b6839bbe840089ff8
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-13 19:00:59 +0200

    Updated code and added arrays

--- src/main/java/cinema/CinemaRoomManager.java
+++ src/main/java/cinema/CinemaRoomManager.java
@@ -11,37 +11,55 @@ public class CinemaRoomManager {
         int totalIncome = 0;
         int rows;
         int seats;
-        int rowNumber;
-        int seatNumber;
+        int rowNumber = 0;
+        int seatNumber = 0;
 
         // Read two positive integer numbers that represent the number of rows and seats in each row.
         System.out.println("Enter the number of rows:");
         rows = input.nextInt();
         System.out.println("Enter the number of seats in each row:");
         seats = input.nextInt();
+        int item;
 
-        // Calls a method that prints rows and seats.
-        printSeats(rows, seats);
+        do {
+            System.out.println("1. Show the seats\n2. Buy a ticket\n0. Exit");
+            item = input.nextInt();
 
-        System.out.println();
+            switch (item) {
+                case 1:
+                    String[][] boughtSeatsArray = new String[rows][seats];
+                    String[][] newBoughtSeatsArray = new String[rows][seats];
+                    if (rowNumber == 0 && seatNumber == 0) {
+                        printSeats(rows, seats); // Calls a method that prints rows and seats.
+                        System.out.println();
+                    } else {
+                        // Calls a method that prints the rows and seats including the purchased seat(s).
+                        newBoughtSeatsArray = ticketsBought(rows, seats, rowNumber, seatNumber, boughtSeatsArray);
+                        printSeatsBought(rows, seats, rowNumber, seatNumber, newBoughtSeatsArray);
+                    }
+                    break;
+                /* Read two integer numbers from the input: a row number and a seat number in that row.
+                 * These numbers represent the coordinates of the seat according to which the program should print the ticket price.
+                 */
+                case 2:
+                    System.out.println("Enter a row number:");
+                    rowNumber = input.nextInt();
+                    System.out.println("Enter a seat number in that row:");
+                    seatNumber = input.nextInt();
+                    // Calls a method that determines the price of tickets.
+                    determineTicketPrice(rowNumber);
 
-        /* Read two integer numbers from the input: a row number and a seat number in that row.
-         * These numbers represent the coordinates of the seat according to which the program should print the ticket price.
-         */
-        System.out.println("Enter a row number:");
-        rowNumber = input.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        seatNumber = input.nextInt();
+                    System.out.println();
 
-        // Calls a method that determines the price of tickets.
-        determineTicketPrice(rowNumber);
 
-        System.out.println();
 
-        // Calls a method that prints the rows and seats including the purchased seat(s).
-        printSeatsBought(rows, seats, rowNumber, seatNumber);
+                    System.out.println();
+                    break;
+                case 0:
+                    break;
+            }
+        } while (item != 0);
 
-        System.out.println();
 
         // Calls method to determine total income.
         totalIncome = findTotalIncome(rows, seats);
@@ -78,8 +96,23 @@ public class CinemaRoomManager {
             System.out.println();
         }
     }
+
+    // A method that determines if a ticket was bought and updates an array
+    public static String[][] ticketsBought(int rows, int seats, int rowNumber, int seatNumber, String[][] seatingArray) {
+        for (int i = 0; i < seats; i++) {
+            for (int j = 0; j < rows; j++) {
+                // Determine if seat is bought (B) or not (S).
+                if (rowNumber == (i + 1) && seatNumber == (j + 1)) {
+                    seatingArray[i][j] = "B";
+                } else {
+                    seatingArray[i][j] = "S";
+                }
+            }
+        }
+        return seatingArray;
+    }
     // A method that prints the seating arrangement with purchased seats.
-    public static void printSeatsBought(int rows, int seats, int rowNumber, int seatNumber) {
+    public static void printSeatsBought(int rows, int seats, int rowNumber, int seatNumber, String[][] newSeatingArray) {
         // Print seat arrangement with bought seats(s).
         System.out.println("Cinema:");
         // Print seat numbers.
@@ -93,16 +126,12 @@ public class CinemaRoomManager {
             System.out.print(i + 1 + " ");
             // Print seats in each row.
             for (int j = 0; j < seats; j++) {
-                // Determine if seat is bought (B) or not (S).
-                if (rowNumber == (i + 1) && seatNumber == (j + 1)) {
-                    System.out.print("B ");
-                } else {
-                    System.out.print("S ");
-                }
+                System.out.print(newSeatingArray[i][j]);
             }
             System.out.println();
         }
     }
+
     // A method that determines the regular and discounted prices for tickets and figures out the total income.
     public static int findTotalIncome(int rows, int seats) {
         // Determine if rows are greater than 4 to apply seat discount.
